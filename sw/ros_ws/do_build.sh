9df1891d5fe66a08ea083d9ef645be9bab3c702c
AndyC <andrew.cappon@gmail.com>
2026-05-10 08:43:57 -0400

    Don't need make install

--- sw/ros_ws/do_build.sh
+++ sw/ros_ws/do_build.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 cd /home/viking/hunker/sw/ros_ws
-source install/setup.bash
-colcon build --packages-select bno08x_ros2_driver
-cd /home/viking/hunker/sw/ros_ws/build/bno08x_ros2_driver
-sudo make install
 
+# First fix ownership of anything root may have clobbered
+sudo chown -R viking:viking /home/viking/hunker/sw/ros_ws/install
+sudo chown -R viking:viking /home/viking/hunker/sw/ros_ws/build
+
+source /opt/ros/jazzy/setup.bash
+colcon build --packages-select bno08x_ros2_driver
+source install/setup.bash
