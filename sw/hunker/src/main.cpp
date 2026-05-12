8363f0eeb22e23081d133bcae5ea38b5d5320054
AndyC <andrew.cappon@gmail.com>
2026-05-11 20:04:15 -0400

    Cleaner shutdown, especially when Ctrl-C

--- sw/hunker/src/main.cpp
+++ sw/hunker/src/main.cpp
@@ -11,12 +11,13 @@ void signal_handler(int signum)
     {
         g_myRobotNode->writeLog("Signal %d received, shutting down...", signum);
         g_myRobotNode->emergencyStop();
+        g_myRobotNode.reset(); // explicitly destroy the node
     }
     catch (const std::exception &e)
     {
         // ignore exception
     }
-    exit(1);
+    rclcpp::shutdown(); // signals spin() to return, then main() cleans up
 }
 
 int main(int argc, char *argv[])
@@ -24,12 +25,17 @@ int main(int argc, char *argv[])
     // Register signal(s) handler
     std::signal(SIGINT, signal_handler);
     std::signal(SIGTERM, signal_handler);
- 
+
     rclcpp::init(argc, argv);
     g_myRobotNode = std::make_shared<RobotNode>();
     g_myRobotNode->init();
 
     rclcpp::spin(g_myRobotNode);
+
+    g_myRobotNode->emergencyStop();
+    g_myRobotNode.reset(); // explicitly destroy the node
+    sleep(1);
+
     rclcpp::shutdown();
     return 0;
 }
